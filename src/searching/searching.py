9594932cdd43a395292d1930cbc194e776834b94
Nathan Loveless <nathansl2003@yahoo.com>
2020-08-10 15:15:06 -0400

    Completed MVP

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,7 +1,8 @@
 def linear_search(arr, target):
     # Your code here
-
-
+    for i in range(len(arr)):
+        if arr[i] == target:
+            return i
     return -1   # not found
 
 
@@ -9,6 +10,17 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
+    min = 0
+    mid = 0
+    high = len(arr) - 1
 
+    while min <= high:
+        mid = (high + min) // 2
+        if arr[mid] < target:
+            min = mid + 1
+        elif arr[mid] > target:
+            high = mid - 1
+        else:
+            return mid
 
     return -1  # not found
